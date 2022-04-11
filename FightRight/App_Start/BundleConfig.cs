1ef17af4e150dbc7e28451eec33833432a6930d3
Tyler Phiffer <tmphiffer@cincinnatistate.edu>
2022-04-11 13:59:08 -0400

    asdf

---
+++ FightRight/App_Start/BundleConfig.cs
@@ -0,0 +1,37 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace FightRight {
+	public class BundleConfig {
+		public static void RegisterBundles(BundleCollection bundles) {
+			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+						"~/Scripts/web2.js",
+						"~/Scripts/modernizr-{version}.js",
+						"~/Scripts/jquery.filedrop.js",
+						"~/Scripts/jquery-{version}.js"));
+
+			bundles.Add(new StyleBundle("~/Content/css").Include(
+						"~/Content/bootstrap.css",
+						"~/Content/bootstrap-utilities.css",
+						"~/Content/styles.css",
+						"~/Content/site.css"
+			));
+
+			
+
+
+		}
+	}
+}
+
+
+//				"~/ Scripts/bootstrap.bundle.js",
+//
+//"~/Scripts/bootstrap.js"
+/*bundles.Add(new StyleBundle("~/bundles/bootstrap").Include(
+						"~/Scripts/bootstrap.bundle.min.js",
+						"~/Scripts/bootstrap.js"
+			));
+ *<script src="Scripts/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+	<script src="Scripts/bootstrap.js"></script>
+ */
---
+++ FightRight/Controllers/FighterSelectController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FightRight.Controllers
+{
+    public class FighterSelectController : Controller
+    {
+        // GET: FighterSelect
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
